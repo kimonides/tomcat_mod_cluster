5ff196a154a636937f22d92c6f7a7c59c45b4a95
Kimonides Alexander <kimonide@gmail.com>
2020-10-26 16:55:05 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ COPY start.sh ./
 
 # EXPOSE 8080
 ENV tomcat_port=8080
+ENV tomcat_shutdown_port=8005
 
 CMD ["./start.sh"]
 
--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 
 sed -i "s/8080/${tomcat_port}/" ./conf/server.xml
 
+sed -i "s/8005/${tomcat_shutdown_port}/" ./conf/server.xml
+
 catalina.sh run
 
